265beb83f0ce072b26f13540e35d8197f7c52253
Dariusz Piwko <darekpiwko@outlook.com>
2017-03-29 21:19:18 +0200

    Create ng-callback

--- app/directive.js
+++ app/directive.js
@@ -1,3 +1,26 @@
-/**
- * Created by darek on 28.03.17.
- */
+(function ()
+{
+    'use strict';
+
+    function ngCall()
+    {
+        function link(scope, element, attrs)
+        {
+            scope.callback(function (data)
+            {
+                console.log(data);
+            });
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                callback: '='
+            },
+            bindToController: true,
+            link: link
+        };
+    }
+
+    angular.module('ngCallback').directive('ngCall', [ngCall]);
+})();
